e8f3927e2bc9ef36a93ea54a841579a72f8eed0a
Sanjana Kumari <sksanjana1502@gmail.com>
2025-07-20 11:07:16 +0530

    Create GCD.py

---
+++ GCD.py
@@ -0,0 +1,12 @@
+# Take input from user
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+gcd = 1
+
+# Loop from 1 to the smallest of the two numbers
+for i in range(1, min(a, b) + 1):
+    if a % i == 0 and b % i == 0:
+        gcd = i  # Update gcd
+
+print(f"The GCD of {a} and {b} is: {gcd}")
