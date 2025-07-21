0d2e5fe519ce6d4e075df9df4f016b07b042647b
Sanjana Kumari <sksanjana1502@gmail.com>
2025-07-21 23:02:06 +0530

    Create Calculation.py

---
+++ Calculation.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Select operation: + - * /")
+op = input("Enter operator: ")
+
+if op == '+':
+    print("Result:", num1 + num2)
+elif op == '-':
+    print("Result:", num1 - num2)
+elif op == '*':
+    print("Result:", num1 * num2)
+elif op == '/':
+    print("Result:", num1 / num2)
+else:
+    print("Invalid operator")
