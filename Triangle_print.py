b43f654050269eb56a7b3694267182afe09162d8
Sanjana Kumari <sksanjana1502@gmail.com>
2025-08-03 23:02:59 +0530

    Update Triangle_print.py

--- Triangle_print.py
+++ Triangle_print.py
@@ -1,8 +1,6 @@
 rows = int(input("Enter the number of rows: "))
 
-# Outer loop for each row
 for i in range(1, rows + 1):
-    # Inner loop for printing stars
     for j in range(1, i + 1):
         print("*", end=" ")
-    print()  # Move to the next line after each row
+    print()  
