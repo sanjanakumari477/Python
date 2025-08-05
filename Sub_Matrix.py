e968ca70871a020ba303f456d26add80dc55ab71
Sanjana Kumari <sksanjana1502@gmail.com>
2025-08-05 23:28:34 +0530

    Create Sub_Matrix.py

---
+++ Sub_Matrix.py
@@ -0,0 +1,26 @@
+matrix1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+matrix2 = [
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]
+]
+
+result = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+
+for i in range(len(matrix1)):
+    for j in range(len(matrix1[0])):
+        result[i][j] = matrix1[i][j] - matrix2[i][j]
+
+# Print the result
+print("Sub of the two matrices:")
+for row in result:
+    print(row)
