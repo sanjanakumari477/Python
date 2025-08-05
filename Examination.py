06efd9de07a805e16d09ee3628f7e05b4bed9bd2
Sanjana Kumari <sksanjana1502@gmail.com>
2025-08-05 23:31:55 +0530

    Create Examination.py

---
+++ Examination.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your age: "))
+if age >= 14:
+    print(" You are eligible for the 10th exam.")
+else:
+    print(" You are NOT eligible for the 10th exam. Minimum age is 14.")
