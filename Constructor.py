1647c86d78160d807194ff0728ed9291421bba51
Sanjana Kumari <sksanjana1502@gmail.com>
2025-08-15 07:41:06 +0530

    Create Constructor.py

---
+++ Constructor.py
@@ -0,0 +1,20 @@
+class Student:
+    def __init__(self, name, roll_no, marks):
+        # Constructor to initialize values
+        self.name = name
+        self.roll_no = roll_no
+        self.marks = marks
+
+    def display(self):
+        print("Student Name:", self.name)
+        print("Roll Number:", self.roll_no)
+        print("Marks:", self.marks)
+
+# Creating objects
+student1 = Student("Sanjana Kumari", 101, 88)
+student2 = Student("Rishi Kumar", 102, 92)
+
+# Display details
+student1.display()
+print()
+student2.display()
