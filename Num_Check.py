33204f0b9a49fbb0b3c23dc8605b55b755317d11
Sanjana Kumari <sksanjana1502@gmail.com>
2025-08-16 22:39:23 +0530

    Create Num_Check.py

---
+++ Num_Check.py
@@ -0,0 +1,10 @@
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("The number is Positive")
+elif number < 0:
+    print("The number is Negative")
+elif number == 0:
+    print("The number is Zero")
+else:
+    print("Invalid input")
