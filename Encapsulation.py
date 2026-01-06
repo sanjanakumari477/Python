e7d0e57a3b1989b55abaa992c5d7b6b431087113
Sanjana Kumari <sksanjana1502@gmail.com>
2026-01-06 21:11:40 +0530

    Update Encapsulation.py

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,11 +1,9 @@
 class Person:
     def __init__(self, age):
         self.__age = age     # name-mangled: _Person__age
-
     @property
     def age(self):
         return self.__age
-
     @age.setter
     def age(self, val):
         if val < 0: raise ValueError("age >= 0")
