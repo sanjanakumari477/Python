e2e902b504de3cd2ba2185fdaa36571b585180b2
Sanjana Kumari <sksanjana1502@gmail.com>
2026-01-26 22:17:20 +0530

    Create Inheritance_Types.py

---
+++ Inheritance_Types.py
@@ -0,0 +1,29 @@
+There are five types of inheritance in Python.
+
+They are:
+
+1.Single Inheritance – One child inherits from one parent
+
+2.Multiple Inheritance – One child inherits from multiple parents
+
+3.Multilevel Inheritance – A class inherits from another derived class
+
+4.Hierarchical Inheritance – Multiple children inherit from one parent
+
+5.Hybrid Inheritance – Combination of two or more types of inheritance
+
+📌 Python supports all five types.
+
+🔹 Q3. What is super() in Python?
+
+super() is a built-in function used to access methods and constructors of the parent (super) class from the child class.
+
+🔹 Why use super()?
+
+To call parent class constructor
+
+To reuse parent class methods
+
+To avoid duplicate code
+
+Useful in multiple inheritance
