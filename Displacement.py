e6cfa837a2f0d58a08ae7e17a5c53cf0b44b3b42
Sanjana Kumari <sksanjana1502@gmail.com>
2026-02-15 23:12:01 +0530

    Update Displacement.py

--- Displacement.py
+++ Displacement.py
@@ -3,6 +3,8 @@
 u = float(input("Enter initial velocity (u) in m/s: "))
 t = float(input("Enter time (t) in seconds: "))
 a = float(input("Enter acceleration (a) in m/s²: "))
+
 # Calculate displacement
+
 s = u * t + 0.5 * a * t * t
 print("Displacement is:", s, "meters")
